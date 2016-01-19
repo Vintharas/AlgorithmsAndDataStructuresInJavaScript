774ae34eceb80997d7793a7fbb3094ca1115db13
Jaime González García <jaime.gg83@gmail.com>
2016-01-19 22:39:20 +0100

    Implemented several operations with linked lists and refactored

--- js/LinkedLists/SimpleLinkedList.js
+++ js/LinkedLists/SimpleLinkedList.js
@@ -1,30 +1,67 @@
 function LinkedList(value){
-
+    let head = Node(value);
 
     return {
-
-        head: Node(value),
-        hasItem(value){
-            var currentItem = this.head;
-
-            while (currentItem){
-                if (currentItem.value === value) return true;
-                currentItem = currentItem.next;
-            }
-
-            return false;
-        },
-        append(value){
-            var lastItem = this.head;
-            while (lastItem.next !== null) lastItem = lastItem.next;
-            lastItem.next = Node(value);
-        }
+        get head(){ return head;},
+        hasItem,
+        append,
+        remove,
+        insert
     };
+
+    function hasItem(value){
+        let currentItem = head;
+
+        // TODO: extract iteraton
+        while (currentItem){
+            if (currentItem.value === value) return true;
+            currentItem = currentItem.next;
+        }
+
+        return false;
+    }
+
+    function append(value){
+        let lastItem = head;
+        while (lastItem.next !== null) lastItem = lastItem.next;
+        lastItem.next = Node(value);
+    }
+
+    function insert(value){
+        let currentHead = head;
+        head = Node(value, /* next */ currentHead);
+    }
+    
+    function remove(value){
+        if (!hasItem(value)) {
+            console.log('here');
+            throw new Error(`Item ${value} is not in the list`);
+        }
+        if (value === head.value) {
+            head = head.next; 
+        } else {
+            removeItemInside(value);
+        }
+    }
+
+    function removeItemInside(value){
+        let previousItem,
+            currentItem = head;
+
+        while (currentItem){
+            if (currentItem.value === value){
+                previousItem.next = currentItem.next;
+            }
+            previousItem = currentItem;
+            currentItem = currentItem.next;
+        }
+    }
+
 }
 
-function Node(value){
+function Node(value, next=null){
     return {
         value,
-        next: null
+        next
     };
 }
--- specs/LinkedLists/SimpleLinkedLists.spec.js
+++ specs/LinkedLists/SimpleLinkedLists.spec.js
@@ -1,3 +1,22 @@
+/*
+ * Linked Lists
+ *
+ * - create linked list with 1 item (head)
+ * - having a list with an item
+ *   - hasItem returns true if the list has that item
+ *   - hasItem returns false if the list doesn't have that item
+ *   - should be able to add an item to an existing list
+ *   - should be able to add multiple items to the list
+ *   - should be able to insert an item at the beginning
+ * - having a list with multiple items
+ *   - hasItem returns true if the list has an item
+ *   - hasItem returns false if the list doesn't have an item
+ *   - remove first item from the list
+ *   - remove an item from the list
+ *   - should throw error when trying to remove an item that doesn't exist
+ *
+ */
+
 describe("Linked Lists", () => {
 
     it("Should be able to create a Linked List from a single item which would be its head", () => {
@@ -49,6 +68,14 @@ describe("Linked Lists", () => {
             expect(head.next.next.value).toBe(6);
             expect(head.next.next.next.value).toBe(8);
         });
+
+        it("should be able to insert another item at the beginning of the list", () => {
+            // Arrange, Act
+            list.insert(1);
+            // Assert
+            var head = list.head;
+            expect(head.value).toBe(1);
+        });
     });
 
     describe("Given that I have a linked list with multiple items", () => {
@@ -72,8 +99,45 @@ describe("Linked Lists", () => {
             });
         });
 
+        it("should be able to remove the first item from the list", () => {
+            // Arrange, Act
+            list.remove(5);
+            // Assert
+            expect(list.hasItem(5)).toBe(false, 'list has item 5');
+            expect(list.hasItem(3)).toBe(true, 'list doesnt have item 3');
+            expect(list.hasItem(8)).toBe(true, 'list doesnt have item 8');
+            expect(list.hasItem(9)).toBe(true, 'list doesnt have item 9');
+        });
+
+        it("should be able to remove any item from the list", () => {
+            // Arrange, Act
+            list.remove(3);
+            // Assert
+            expect(list.hasItem(5)).toBe(true, 'list doesnt have item 5');
+            expect(list.hasItem(3)).toBe(false, 'list has item 3');
+            expect(list.hasItem(8)).toBe(true, 'list doesnt have item 8');
+            expect(list.hasItem(9)).toBe(true, 'list doesnt have item 9');
+        });
+
+        it("should throw an error if you try to remove an item that is not in the list", () => {
+            // Arrange, Act, Assert
+            expect(() => list.remove(111)).toThrowError();
+        });
+
 
     
     });
+
+});
+
+/*
+ * Implement a Stack using a linked list (LIFO data structure)
+ *
+ * - pop
+ * - push
+ *
+ */
+
+describe("Stack", () => {
 
 });
