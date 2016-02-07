abc15e2478e17988039c510844bee7e331faa462
Jaime González García <jaime.gg83@gmail.com>
2016-02-07 22:13:57 +0100

    Added a string matching implementation using BoyerMooreHoorst

--- js/strings/stringMatches.js
+++ js/strings/stringMatches.js
@@ -6,11 +6,23 @@
  */
 
 
-class StringMatcher{
-   findMatches(text, term){
-       if (term === '') return StringMatcher.NoMatches
-       if (term === null || term === undefined) throw new Error("Invalid term")
+class StringMatcher {
+    constructor(strategy){
+        this.strategy = strategy
+    }
+    findMatches(text, term){
+        if (term === '') return StringMatcher.NoMatches    
+        if (term === null || term === undefined) throw new Error("Invalid term");
 
+        return this.strategy.findMatches(text, term);
+    }
+
+}
+StringMatcher.NoMatches = Object.freeze([]);
+
+
+class FindMatchesNaively{
+   findMatches(text, term){
        const matches = [];
        for(let i = 0; i <= text.length - term.length; i++)
            if (this.findMatchAtIndex(i, text, term)) matches.push(new StringMatch(term, i));
@@ -26,10 +38,8 @@ class StringMatcher{
 
        return foundMatch
    }
+
 }
-
-StringMatcher.NoMatches = Object.freeze([]);
-
 
 class StringMatch{
     constructor(term, index){
@@ -37,3 +47,68 @@ class StringMatch{
         this.index = index;
     }
 }
+
+class FindMatchesWithBoyerMooreHoorst {
+
+    findMatches(text, term) {
+        l.log(`attempting to find ${term} inside ${text}`); 
+
+        const matches = [];
+        const jumpsTable = this.buildTermJumpingTable(term);
+        for(let i = 0; i < text.length - term.length; i++) {
+            // compare from the last term letter
+            l.log(`searching at index ${i}`); 
+
+            for(let j = i + term.length - 1; j >= i; j--) {
+                l.log(`attempting match between term ${term[j-i]} and ${text[j]}`); 
+                if (text[j] === term[j-i]){
+                    l.log(`matched letter ${text[j]} with ${term[j-i]}`); 
+                    // match!
+                    if (j-i === 0) {
+                        // we are matching the last letter
+                        // so we've got a match
+                        l.log(`FOUND MATCH!!`); 
+                        matches.push(new StringMatch(term, i))
+                        i = i + term.length - 1;
+                    }
+                } else {
+                    // no match => jump!
+                    l.log(`no match`); 
+                    if (jumpsTable.has(text[j])){
+                        let jumps = jumpsTable.get(text[j])
+                        l.log(`Found ${text[j]} must jump ${jumps}`); 
+                        i = i + jumps;
+                    } else {
+                        l.log(`Didn't find ${text[j]} must jump ${term.length}`); 
+                        i = i + term.length - 1;
+                    }
+                    break;
+                }
+            }
+        }
+
+        return matches;
+    }
+
+    buildTermJumpingTable(term){                           
+        // TRUTH
+        // any word => 5
+        //   store how many jumps you need to do when 
+        //   failing a compare
+        // T => 5 - 0 - 1 = 4
+        // R => 5 - 1 - 1 = 3
+        // U => 5 - 2 - 1 = 2
+        // T => 5 - 3 - 1 = 1
+        // H => 5
+        const jumpsTable = new Map()
+        for(let i = 0; i < term.lenght -1; i++){
+            jumpsTable.set(letter, term.length - i - 1)
+        }
+        return jumpsTable;
+    }
+}
+
+
+
+
+
--- specs/strings/findStringMatches.spec.js
+++ specs/strings/findStringMatches.spec.js
@@ -6,49 +6,96 @@
  */
 
 describe("StringMatcher", () => {
-    describe("Given an arbitrary text", () => {
-        const text = "Once upon a time there was a princess";
-        let matcher;
 
-        beforeEach(() => matcher = new StringMatcher());
-    
-        describe("And an empty term to match", () => {
-            it("Should return no matches", () => {
-                // Arrange, Act
-                const matches = matcher.findMatches(text, '');
-                // Assert
-                expect(matches.length).toBe(0);
+    describe("Naive string matching", () => {
+        describe("Given an arbitrary text", () => {
+            const text = "Once upon a time there was a princess";
+            let matcher;
+
+            beforeEach(() => matcher = new StringMatcher(new FindMatchesNaively));
+        
+            describe("And an empty term to match", () => {
+                it("Should return no matches", () => {
+                    // Arrange, Act
+                    const matches = matcher.findMatches(text, '');
+                    // Assert
+                    expect(matches.length).toBe(0);
+                });
+            });
+
+            describe("And a null term to match", () => {
+                it("Should throw an error", () => {
+                    // Arrange, Act, Assert
+                    expect(() => matcher.findMatches(text, null)).toThrowError();
+                });
+            });
+
+            describe("And an undefined term to match", () => {
+                it("Should throw an error", () => {
+                    // Arrange, Act, Assert
+                    expect(() => matcher.findMatches(text)).toThrowError();
+                });
+            });
+
+            describe("And a matching term", () => {
+                it("Should return a match", () => {
+                    // Arrange, Act
+                    const matches = matcher.findMatches(text, 'time');
+                    // Assert
+                    expect(matches.length).toBe(1);
+                    const match = matches[0];
+                    expect(match.term).toBe('time');
+                    expect(match.index).toBe(12);
+                });
             });
         });
+    });
 
-        describe("And a null term to match", () => {
-            it("Should throw an error", () => {
-                // Arrange, Act, Assert
-                expect(() => matcher.findMatches(text, null)).toThrowError();
+    describe("BoyerMooreHoorst string matching", () => {
+
+        describe("Given an arbitrary text", () => {
+            const text = "Once upon a time there was a princess";
+            let matcher;
+
+            beforeEach(() => matcher = new StringMatcher(new FindMatchesWithBoyerMooreHoorst()));
+        
+            describe("And an empty term to match", () => {
+                it("Should return no matches", () => {
+                    // Arrange, Act
+                    const matches = matcher.findMatches(text, '');
+                    // Assert
+                    expect(matches.length).toBe(0);
+                });
+            });
+
+            describe("And a null term to match", () => {
+                it("Should throw an error", () => {
+                    // Arrange, Act, Assert
+                    expect(() => matcher.findMatches(text, null)).toThrowError();
+                });
+            });
+
+            describe("And an undefined term to match", () => {
+                it("Should throw an error", () => {
+                    // Arrange, Act, Assert
+                    expect(() => matcher.findMatches(text)).toThrowError();
+                });
+            });
+
+            describe("And a matching term", () => {
+                beforeEach(() => l.isEnabled = true);
+                it("Should return a match", () => {
+                    // Arrange, Act
+                    const matches = matcher.findMatches(text, 'time');
+                    // Assert
+                    expect(matches.length).toBe(1);
+                    const match = matches[0];
+                    expect(match.term).toBe('time');
+                    expect(match.index).toBe(12);
+                });
+                afterEach(() => l.isEnabled = false);
             });
         });
-
-        describe("And an undefined term to match", () => {
-            it("Should throw an error", () => {
-                // Arrange, Act, Assert
-                expect(() => matcher.findMatches(text)).toThrowError();
-            });
-        });
-
-        describe("And a matching term", () => {
-            beforeEach(() => l.isEnabled = true);
-            it("Should return a match", () => {
-                // Arrange, Act
-                const matches = matcher.findMatches(text, 'time');
-                // Assert
-                expect(matches.length).toBe(1);
-                const match = matches[0];
-                expect(match.term).toBe('time');
-                expect(match.index).toBe(12);
-            });
-            afterEach(() => l.isEnabled = false);
-        });
-    
     
     });
 });
